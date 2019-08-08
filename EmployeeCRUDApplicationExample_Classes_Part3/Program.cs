995568e735e02582e4a68a89bd6a6a0af2558f4c
Lon <gavinlondee@gmail.com>
2019-08-08 17:06:37 +0200

    Initial commit

---
+++ EmployeeCRUDApplicationExample_Classes_Part3/Program.cs
@@ -0,0 +1,24 @@
+﻿using EmployeeComponent;
+using EmployeeComponent.Data;
+using EmployeeComponent.Views;
+using System;
+
+namespace EmployeeCRUDApplicationExample_Classes_Part3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employees employees = new Employees();
+
+            EmployeeRecordsView employeeRecordsView = new EmployeeRecordsView(employees);
+
+            Console.WriteLine(EmployeeCommonOutputText.GetApplicationHeading());
+
+            employeeRecordsView.RunRecordsView();
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ EmployeeComponent/EmployeeObjectFactory.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using EmployeeComponent.Data;
+using EmployeeComponent.Views;
+
+namespace EmployeeComponent
+{
+    public static class EmployeeObjectFactory
+    {
+        public static Employee CreateNewEmployeeObject(string firstName, string lastName, decimal annualSalary, char gender, bool isManager)
+        {
+            return new Employee
+            {
+                FirstName = firstName,
+                LastName = lastName,
+                AnnualSalary = annualSalary,
+                Gender = gender,
+                IsManger = isManager
+            };
+
+        }
+
+        public static EmployeeRecordsView EmployeeRecordsViewObject(Employees employees)
+        {
+            return new EmployeeRecordsView(employees);
+        }
+
+
+    }
+
+}
